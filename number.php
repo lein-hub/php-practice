74ce5f23eb3da5cc781c463da27c7db6189a4b6a
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-23 19:32:19 +0900

    number

---
+++ number.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Number & Arithmetic Operator</h1>
+    <h2>1+1</h2>
+    <?php
+     echo 1;
+     print(1);
+     // 둘다 똑같다
+     echo 1+1; // 2
+    ?>
+    <h2>2-1</h2>
+    <?php
+    echo 2-1;
+     ?>
+     <h2>2*2</h2>
+     <?php
+     echo 2*2;
+      ?>
+      <h2>4/2</h2>
+      <?php
+      echo 4/2; ?>
+  </body>
+</html>
