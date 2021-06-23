7b2b9c644ecbcfffabb8c0a8614cb0f20083e119
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-23 19:45:52 +0900

    string and string operator

---
+++ string.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>String & String Operator</h1>
+    <?php
+    echo 'Hello world'; ?>
+    <h2>concatenation operator</h2>
+    <?php
+    echo "Hello "."world"; ?>
+    <h2>String length function</h2>
+    <?php
+    echo strlen("Hello world"); ?>
+  </body>
+</html>
