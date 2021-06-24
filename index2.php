6f55d882f70251394c9f6bbc6d02fe39a46b3e10
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-24 15:45:33 +0900

    isset

--- index2.php
+++ index2.php
@@ -5,7 +5,7 @@
     <title></title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index2.php">WEB</a></h1>
     <ol>
       <li><a href="index2.php?id=HTML">HTML</a></li>
       <li><a href="index2.php?id=CSS">CSS</a></li>
@@ -13,11 +13,18 @@
     </ol>
     <h2>
       <?php
-        echo $_GET['id'];
+        if (isset($_GET['id']))
+          echo $_GET['id'];
+        else
+          echo "Welcome";
        ?>
     </h2>
     <?php
-    $filename = $_GET['id'];
-    echo file_get_contents("data/".$filename);
+      if (isset($_GET['id'])) {
+        $filename = $_GET['id'];
+        echo file_get_contents("data/".$filename);
+      } else {
+        echo "Hello, PHP";
+      }
      ?>
 </html>
