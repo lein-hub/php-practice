8a2b3ad7ae0da32bee9c3c04b3fdd25a6a6daf7f
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-24 20:48:26 +0900

    require

--- create.php
+++ create.php
@@ -1,49 +1,7 @@
 <?php
-function print_title() {
-  if (isset($_GET['id']))
-    echo $_GET['id'];
-  else
-    echo "Welcome";
-}
-
-function print_description() {
-  if (isset($_GET['id'])) {
-    $filename = $_GET['id'];
-    echo file_get_contents("data/".$filename);
-  } else {
-    echo "Hello, PHP";
-  }
-}
-
-function print_list() {
-  $list = scandir('./data');
-  $i = 0;
-  while ($i < count($list)) {
-    if ($list[$i] != "." && $list[$i] != "..") {
-      echo "<li><a href=\"index2.php?id=$list[$i]\">$list[$i]</a></li>\n";
-    }
-    $i = $i + 1;
-  }
-}
- ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php
-        print_title();
-       ?>
-    </title>
-  </head>
-  <body>
-    <h1><a href="index2.php">WEB</a></h1>
-    <ol>
-      <?php
-        print_list();
-       ?>
-    </ol>
+require_once('./lib/print.php');
+require('./view/top.php');
+?>
     <a href="create.php">create</a>
     <form action="create_process.php" method="post">
       <p>
@@ -56,5 +14,6 @@ function print_list() {
         <input type="submit">
       </p>
     </form>
-  </body>
-</html>
+    <?php
+    require('./view/bottom.php');
+     ?>
--- index2.php
+++ index2.php
@@ -1,51 +1,9 @@
 <?php
-function print_title() {
-  if (isset($_GET['id']))
-    echo $_GET['id'];
-  else
-    echo "Welcome";
-}
-
-function print_description() {
-  if (isset($_GET['id'])) {
-    $filename = $_GET['id'];
-    echo file_get_contents("data/".$filename);
-  } else {
-    echo "Hello, PHP";
-  }
-}
-
-function print_list() {
-  $list = scandir('./data');
-  $i = 0;
-  while ($i < count($list)) {
-    if ($list[$i] != "." && $list[$i] != "..") {
-      echo "<li><a href=\"index2.php?id=$list[$i]\">$list[$i]</a></li>\n";
-    }
-    $i = $i + 1;
-  }
-}
- ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php
-        print_title();
-       ?>
-    </title>
-  </head>
-  <body>
-    <h1><a href="index2.php">WEB</a></h1>
-    <ol>
-      <?php
-        print_list();
-       ?>
-    </ol>
+require_once('./lib/print.php');
+require('./view/top.php');
+?>
     <a href="create.php">create</a>
-    <?php if (isset($_GET['id'])) { ?>
+  <?php if (isset($_GET['id'])) { ?>
     <a href="update.php?id=<?=$_GET['id']?>">update</a>
     <form class="" action="delete_process.php" method="post">
       <input type="hidden" name="id" value="<?=$_GET['id']?>">
@@ -60,5 +18,6 @@ function print_list() {
     <?php
       print_description();
      ?>
-   </body>
-</html>
+<?php
+require('./view/bottom.php');
+ ?>
