65c71cd7ef195afa96654e4dc84f532f3836eb85
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-24 14:26:20 +0900

    parameter

---
+++ parameter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- $_GET['~']으로 쿼리값을 가져올 수 있다. -->
+    안녕하세요. <?php echo $_GET['name']; ?>님
+  </body>
+</html>
