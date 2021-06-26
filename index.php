8b22460fa991e3e86509fde65a0eacb25583621c
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-06-26 17:54:51 +0900

    second step

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
   <body>
-    <?php
-      echo date('Y-m-d H:i:s');
-    ?>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+    </p>
   </body>
 </html>
